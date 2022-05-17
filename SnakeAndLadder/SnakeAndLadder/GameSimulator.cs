5159149b5062da647547fb2a8a4ab9fb414ddd21
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-17 14:39:00 +0530

    [Elavarasu] UC4-WinningPosition_100

--- SnakeAndLadder/SnakeAndLadder/GameSimulator.cs
+++ SnakeAndLadder/SnakeAndLadder/GameSimulator.cs
@@ -10,30 +10,36 @@ namespace Game
     {
         public void DieRoller()
         {
-            int player1Position = 0, noPlay=0, snake=-1, ladder=1, dieValueCheck, optionCheck;
+            int player1Position, winningPosition=100, noPlay=0, snake=-1, ladder=1, dieValueCheck, optionCheck;
             Random value=new Random();
-            optionCheck = value.Next(-1, 2);
-            Console.WriteLine("Player option is:" + optionCheck);
-            dieValueCheck = value.Next(1, 7);
-            Console.WriteLine("Dice Rolled to:" + dieValueCheck);
+            
+            for(player1Position=0; player1Position <= winningPosition;)
+            {
+                optionCheck = value.Next(-1, 2);
+                Console.WriteLine("Player option is:" + optionCheck);
+                dieValueCheck = value.Next(1, 7);
+                Console.WriteLine("Dice Rolled to:" + dieValueCheck);
 
-            if (optionCheck == ladder)
-            {
-                player1Position = player1Position + dieValueCheck;
-            }
-            else if(optionCheck == snake)
-            {
-                player1Position = player1Position - dieValueCheck;
-                if(player1Position < 0)
+                if (optionCheck == ladder)
                 {
-                    player1Position = 0;
+                    player1Position = player1Position + dieValueCheck;
                 }
+                else if (optionCheck == snake)
+                {
+                    player1Position = player1Position - dieValueCheck;
+                    if (player1Position < 0)
+                    {
+                        player1Position = 0;
+                    }
+                }
+                else
+                {
+                    player1Position = player1Position + noPlay;
+                }
+                Console.WriteLine("Player position is:" + player1Position);
             }
-            else
-            {
-                player1Position = player1Position + noPlay;
-            }
-            Console.WriteLine("Player position is:" + player1Position);
+           
+            
         }
     }
 }
