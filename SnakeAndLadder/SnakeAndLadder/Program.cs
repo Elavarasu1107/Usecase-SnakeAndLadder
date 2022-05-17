aae9cc2eef9a3f50a084dc8a566434d3e7b9ce41
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-17 12:50:49 +0530

    [Elavarasu] UC1-PlayerStartingPosition

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game
+{
+    internal class SnakeAndLadder
+    {
+        public static void Main(String[] args)
+        {
+            GameSimulator getMethod=new GameSimulator();
+            getMethod.DieRoller();
+        }
+    }
+}
