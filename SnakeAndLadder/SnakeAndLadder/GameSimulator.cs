89a64a682a6f680dc0adb72d355abbdfad236c41
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-18 08:32:28 +0530

    [Elavarasu] UC7-Adding2Players

--- SnakeAndLadder/SnakeAndLadder/GameSimulator.cs
+++ SnakeAndLadder/SnakeAndLadder/GameSimulator.cs
@@ -10,42 +10,88 @@ namespace Game
     {
         public void DieRoller()
         {
-            int playerPosition, winningPosition=100, noPlay=0, snake=-1, ladder=1, dieValueCheck, optionCheck, numOfTimesDiceRolled=1;
-            Random value=new Random();
-            
-            for(playerPosition=0; playerPosition < winningPosition; numOfTimesDiceRolled++)
-            {
-                optionCheck = value.Next(-1, 2);
-                Console.WriteLine("Player option is:" + optionCheck);
-                dieValueCheck = value.Next(1, 7);
-                Console.WriteLine("Dice Rolled to:" + dieValueCheck);
+            int player1Position = 0, player2Position = 0, winningPosition = 100, noPlay = 0, snake = -1, ladder = 1, dieValueCheck, optionCheck, numOfTimesDiceRolled, playerTurn = 0, player1DiceRoll = 0, player2DiceRoll = 0;
+            Random value = new Random();
 
-                if (optionCheck == ladder)
+            while (player1Position < winningPosition && player2Position < winningPosition)
+            {
+
+                optionCheck = value.Next(-1, 2);
+                //Console.WriteLine("Player option is:" + optionCheck);
+                dieValueCheck = value.Next(1, 7);
+                //Console.WriteLine("Dice Rolled to:" + dieValueCheck);
+
+                if (playerTurn == 0)
                 {
-                    playerPosition = playerPosition + dieValueCheck;
-                }
-                else if (optionCheck == snake)
-                {
-                    playerPosition = playerPosition - dieValueCheck;
-                    if (playerPosition < 0)
+                    playerTurn = playerTurn + 1;
+                    player1DiceRoll++;
+                    if (optionCheck == ladder)
                     {
-                        playerPosition = 0;
+                        playerTurn = playerTurn - 1;
+                        player1Position = player1Position + dieValueCheck;
+                    }
+                    else if (optionCheck == snake)
+                    {
+                        player1Position = player1Position - dieValueCheck;
+                        if (player1Position < 0)
+                        {
+                            player1Position = 0;
+                        }
+                    }
+                    else
+                    {
+                        player1Position = player1Position + noPlay;
+                    }
+                    Console.WriteLine("Player 1 position is:" + player1Position);
+                    if (player1Position > 100)
+                    {
+                        player1Position = player1Position - dieValueCheck;
                     }
                 }
                 else
                 {
-                    playerPosition = playerPosition + noPlay;
-                }
-                Console.WriteLine("Player position is:" + playerPosition);
-                if (playerPosition > 100)
-                {
-                    playerPosition = playerPosition - dieValueCheck;
+                    playerTurn = playerTurn - 1;
+                    player2DiceRoll++;
+                    if (optionCheck == ladder)
+                    {
+                        playerTurn = playerTurn + 1;
+                        player2Position = player2Position + dieValueCheck;
+                    }
+                    else if (optionCheck == snake)
+                    {
+                        player2Position = player2Position - dieValueCheck;
+                        if (player2Position < 0)
+                        {
+                            player2Position = 0;
+                        }
+                    }
+                    else
+                    {
+                        player2Position = player2Position + noPlay;
+                    }
+                    Console.WriteLine("Player 2 position is:" + player2Position);
+                    if (player2Position > 100)
+                    {
+                        player2Position = player2Position - dieValueCheck;
+                    }
                 }
 
             }
             Console.WriteLine();
-            Console.WriteLine("No of times Dice rolled to win the game:" + numOfTimesDiceRolled);
+            Console.WriteLine("No of times Player 1 rolled the Dice:" + player1DiceRoll);
+            Console.WriteLine("No of times Player 2 rolled the Dice:" + player2DiceRoll);
+            numOfTimesDiceRolled = player1DiceRoll + player2DiceRoll;
             Console.WriteLine();
+            Console.WriteLine("No of times Dice rolled by both Players:" + numOfTimesDiceRolled);
+            Console.WriteLine();
+            if (player1Position > player2Position)
+            {
+                Console.WriteLine("Player 1 Won the Game");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 Won the Game");
+            }
         }
     }
 }
